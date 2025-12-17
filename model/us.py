7a4a140f481e6e67f1687c72c849342a850c4f1f
ramilhagverdiyev <ramil.ax2503@gmail.com>
2025-12-17 23:49:10 +0400

    project done

---
+++ model/us.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def about_us():
+    st.title('About Us')
+    st.write("""
+    CPP (Car Price Predictor) app was created to predict car prices based on various features using 
+    machine learning algorithms, which will try its best for giving you best prices to you for calculating your 
+    budget for affording a car based on its details that you will provide.
+    """)
+    st.write("**Acknowledgments:** We would like to thank Streamlit and the open-source community for their contributions.")
